f6edbed88b94d08621ea9b8f7787da92ed16c053
Susan Dwyer <suzycream999@googlemail.com>
2024-09-29 16:12:01 +0100

    code cleanup

--- FFMpeg_Progress/Form1.Designer.cs
+++ FFMpeg_Progress/Form1.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
-            this.lblProgress1 = new System.Windows.Forms.Label();
-            this.lblProgress2 = new System.Windows.Forms.Label();
-            this.lblProgress3 = new System.Windows.Forms.Label();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.progress1 = new ProgressLabel.Progress();
             this.SuspendLayout();
             // 
@@ -45,40 +41,6 @@
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
-            // lblProgress1
-            // 
-            this.lblProgress1.Location = new System.Drawing.Point(236, 51);
-            this.lblProgress1.Name = "lblProgress1";
-            this.lblProgress1.Size = new System.Drawing.Size(314, 16);
-            this.lblProgress1.TabIndex = 1;
-            this.lblProgress1.Text = "progress";
-            this.lblProgress1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblProgress2
-            // 
-            this.lblProgress2.Location = new System.Drawing.Point(236, 67);
-            this.lblProgress2.Name = "lblProgress2";
-            this.lblProgress2.Size = new System.Drawing.Size(314, 16);
-            this.lblProgress2.TabIndex = 2;
-            this.lblProgress2.Text = "progress1";
-            this.lblProgress2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblProgress3
-            // 
-            this.lblProgress3.Location = new System.Drawing.Point(236, 83);
-            this.lblProgress3.Name = "lblProgress3";
-            this.lblProgress3.Size = new System.Drawing.Size(314, 16);
-            this.lblProgress3.TabIndex = 3;
-            this.lblProgress3.Text = "progress2";
-            this.lblProgress3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // progressBar
-            // 
-            this.progressBar.Location = new System.Drawing.Point(239, 131);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(311, 23);
-            this.progressBar.TabIndex = 4;
             // 
             // progress1
             // 
@@ -95,10 +57,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.progress1);
-            this.Controls.Add(this.progressBar);
-            this.Controls.Add(this.lblProgress3);
-            this.Controls.Add(this.lblProgress2);
-            this.Controls.Add(this.lblProgress1);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -110,10 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Label lblProgress1;
-        private System.Windows.Forms.Label lblProgress2;
-        private System.Windows.Forms.Label lblProgress3;
-        private System.Windows.Forms.ProgressBar progressBar;
         private ProgressLabel.Progress progress1;
     }
 }
--- FFMpeg_Progress/Form1.cs
+++ FFMpeg_Progress/Form1.cs
@@ -26,27 +26,12 @@ namespace FFMpeg_Progress
 
         public void ProgressChanged(int val)
         {
-            //progressBar.Value = val;
             progress1.Value = val;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //FFMpeg.ExtractionInProgress += FFMpeg_ItemComplete;
-
-
-            //Debug.WriteLine(10.ToString() + " percent of " + 1000.ToString() + " = " + 10.PercentOf(1000));
-            
-            //Debug.WriteLine(10.PercentOf(1000));
-
-            ConversionProgress = new Progress<int>(ProgressChanged);
-
-            Labels = new List<Label>
-            {
-                lblProgress1,
-                lblProgress2,
-                lblProgress3
-            };
+           ConversionProgress = new Progress<int>(ProgressChanged);
 
             BookMarks = new List<BookMark>
             {
@@ -63,19 +48,10 @@ namespace FFMpeg_Progress
         private void FFMpeg_ItemComplete(object sender, (int, int) e)
         {
             string lableText = $"{e.Item1} of {e.Item2}";
-            lblProgress3.Invoke(new Action(() => lblProgress3.Text = lableText));
-            //lblProgress3.Invoke(new Action(() => lblProgress1.Progress(e.Item1)));
-
         }
 
         private async void btnStart_Click(object sender, EventArgs e)
         {
-            //var targetFile = @"C:\Test\copied.mp4";
-            //for (int i = 0; i <= 50; i++)
-            //{
-            //    progress1.Value = i.PercentOf(progress1.Width);
-            //}
-            
             await Task.Run(()=> FFMpeg.ExtractSegments(BookMarks, @"C:\Test\copied.mp4", ConversionProgress));
             progress1.Value = 0;
          }
@@ -85,10 +61,6 @@ namespace FFMpeg_Progress
     {
         public static int PercentOf(this int percent, int of)
         {
-            //Debug.WriteLine("######");
-            //Debug.WriteLine(percent);
-            //Debug.WriteLine(of);
-            //Debug.WriteLine("######");
             return (percent * of) / 100;
         }
     }
